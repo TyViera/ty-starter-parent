753d6a1fee31364aff3fde3a44865fdd3120a974
Nazaret Viera Ipanaque <nazaret.vieraipanaque@tcs.com>
2019-11-07 18:19:21 -0500

    feat: initial project configuration

---
+++ src/main/java/pe/ty/tystarterparent/TyStarterParentApplication.java
@@ -0,0 +1,13 @@
+package pe.ty.tystarterparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyStarterParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyStarterParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/ty/tystarterparent/TyStarterParentApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.ty.tystarterparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyStarterParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
